a9d74e9356b1530934778efa0a3199720980fbd5
Banderce <102034385+Banderce@users.noreply.github.com>
2022-03-22 23:45:24 +1100

    Update Sbond.sol

--- Sbond.sol
+++ Sbond.sol
@@ -14,16 +14,16 @@ ______________ ______ ________ ___________
 */
 
 
-contract GBond is ERC20Burnable, Operator {
+contract SBond is ERC20Burnable, Operator {
     /**
-     * @notice Constructs the GRAPE Bond ERC-20 contract.
+     * @notice Constructs the SDOLLAR Bond ERC-20 contract.
      */
-    constructor() public ERC20("GBOND", "GBOND") {}
+    constructor() public ERC20("SBOND", "SBOND") {}
 
     /**
-     * @notice Operator mints grape bonds to a recipient
+     * @notice Operator mints sdollar bonds to a recipient
      * @param recipient_ The address of recipient
-     * @param amount_ The amount of grape bonds to mint to
+     * @param amount_ The amount of sdollar bonds to mint to
      * @return whether the process has been done
      */
     function mint(address recipient_, uint256 amount_) public onlyOperator returns (bool) {
